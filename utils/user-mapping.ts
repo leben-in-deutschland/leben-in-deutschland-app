d76b8199fbc1653985afa3926c2809adce50ac70
manishtiwari25 <manish.ertiwari@outlook.com>
2024-06-18 18:43:13 +0200

    fixed build error

--- utils/user-mapping.ts
+++ utils/user-mapping.ts
@@ -11,7 +11,6 @@ export const createUserStats = async (user: User, correctAnswer: boolean, option
     if (user.dailyProgress === undefined) {
         user.dailyProgress = [];
     }
-    user.lastQuestionNum = lastQuestionNum;
 
     let todayProgressIndex = user.dailyProgress.findIndex(x => x.date === today);
     if (todayProgressIndex >= 0) {
@@ -30,36 +29,10 @@ export const createUserStats = async (user: User, correctAnswer: boolean, option
             incorrect: correctAnswer ? 0 : 1
         });
     }
-    if (!user.overallProgress) {
-        user.overallProgress = {
-            attempted: 0,
-            correct: 0,
-            incorrect: 0,
-            mockAttempted: 0,
-            mockFailed: 0,
-            mockPassed: 0,
-            flagged: 0,
-            skipped: 0
-        };
-    }
 
     let indexOfUserProgress = user.questionProgress.findIndex(x => x.num === lastQuestionNum);
     if (indexOfUserProgress > -1) {
         let tempData = user.questionProgress[indexOfUserProgress];
-        if (tempData.flagged) {
-            user.overallProgress.flagged = user.overallProgress.flagged + 1;
-        }
-        if (tempData.skipped) {
-            user.overallProgress.skipped = user.overallProgress.skipped + 1;
-        }
-        if (tempData.answerSelected && tempData.answeredCorrectly) {
-            user.overallProgress.correct = user.overallProgress.correct + 1;
-        }
-        if (tempData.answerSelected && !tempData.answeredCorrectly) {
-            user.overallProgress.incorrect = user.overallProgress.incorrect + 1;
-        }
-        user.overallProgress.attempted = user.overallProgress.attempted + 1;
-
         if (tempData.num.startsWith(user.state.stateCode)) {
             user.state.attempted = user.state.attempted + 1;
             if (tempData.answerSelected && tempData.answeredCorrectly) {
