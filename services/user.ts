1d03858b4ca0ab3327ec6cbf18a444ccf40ce6d4
manishtiwari25 <manish.ertiwari@outlook.com>
2024-06-19 15:36:15 +0000

    read user from db after signin

--- services/user.ts
+++ services/user.ts
@@ -1,10 +1,12 @@
 import { User } from "@/types/user";
 import { readFromlocalStorage, saveInlocalStorage } from "@/utils/local-storage";
 
-export const getUserData = async (isAuthenticated: boolean) => {
-    let user = readFromlocalStorage();
-    if(user){
-        return user;
+export const getUserData = async (isAuthenticated: boolean, skipLocal: boolean = false) => {
+    if (!skipLocal) {
+        let user = readFromlocalStorage();
+        if (user) {
+            return user;
+        }
     }
     if (isAuthenticated) {
         const response = await fetch('/api/user');
@@ -12,6 +14,7 @@ export const getUserData = async (isAuthenticated: boolean) => {
             return null;
         }
         const user = await response.json() as User;
+        saveInlocalStorage(user)
         return user;
     }
     return null;
