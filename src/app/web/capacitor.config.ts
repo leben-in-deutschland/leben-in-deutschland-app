a85c7d388823e064a8dfffcaa88beae8deb2de0a
manishtiwari25 <manish.ertiwari@outlook.com>
2025-02-01 14:18:39 +0100

    chore: upload correct apk

--- src/app/web/capacitor.config.ts
+++ src/app/web/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'org.lebenindeutschland.app',
-  appName: 'Einbürgerungstest (No Ads)',
+  appName: 'Einbürgerungstest',
   webDir: 'out',
   server: {
     androidScheme: "https",
